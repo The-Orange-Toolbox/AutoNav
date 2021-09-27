91dee23e5f7b6b980c795a4388f31ef88b482ce9
Maxime Dupuis <mdupuis@hotmail.ca>
2021-09-27 07:33:41 -0400

    skipped waiting for map loads when adequate

--- src/nav_generate.py
+++ src/nav_generate.py
@@ -1,17 +1,19 @@
 from valveexe import ValveExe
+from valveexe.console import RconConsole
 from totcommon.logger import stdout
 
 def nav_generate(mapName, gameExe, gameDir, steamExe, steamId):
     valveExe = ValveExe(gameExe, gameDir, steamExe, steamId)
     stdout('Launching game...')
-
     valveExe.launch(['-windowed', '-novid', '-nosound', 
                      '+sv_cheats', '1', '+map', mapName])
-    stdout('Waiting for map load...')
-
-    valveExe.logger.log_until('Redownloading all lightmaps|connected\.')
 
     with valveExe as console:
+        if not isinstance(console, RconConsole):
+            stdout('Waiting for map load...')
+            map_loaded = 'Redownloading all lightmaps|connected\.'
+            valveExe.logger.log_until(map_loaded)
+
         stdout('Generating Navigation Mesh...')
 
         console.run("nav_generate")
